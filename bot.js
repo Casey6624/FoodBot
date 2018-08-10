ab0dfa918ae84336b4449eb8c425e551c5a62270
Casey Smith <Casey@jollyit.co.uk>
2018-08-10 17:13:56 +0100

    First Commit

---
+++ bot.js
@@ -0,0 +1,12 @@
+console.log("Bot starting");
+
+let Twit = require("twit");
+
+var T = new Twit({
+  consumer_key:         'Wrw1OFQtWmAV3j64JkGejV6q7',
+  consumer_secret:      'NXqtzkWQckpFta36NHY6v8ffQUS4r4gKELrs8hGM3icKMzlhIu',
+  access_token:         '1022191445908877313-EV4o5XVquD6Jfz40B4vgTskBbBnuio',
+  access_token_secret:  'erIn6VYATGI2wrjbwiFHoB9F1n8lygMb9g6ln5uxXVPfX',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
